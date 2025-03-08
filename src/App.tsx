2674fd188a8a63a62b20c9142ab773e84decb1b0
Aditya Dasika <akhil.dasika47@gmail.com>
2025-03-08 20:30:52 +0530

    cors setup and job request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { lazy } from "react"
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/home/index";
-import AnimatedCursor from 'react-animated-cursor';
 
 function App() {
     
@@ -18,19 +17,6 @@ function App() {
                 <Route path="/projects" element={<Project />} />
                 <Route path="*"  element={<Error />} />
             </Routes>
-            <AnimatedCursor
-                innerSize={8}
-                outerSize={35}
-                innerScale={1}
-                outerScale={2}
-                outerAlpha={0}
-                innerStyle={{
-                    backgroundColor: '#A54B0A'
-                }}
-                outerStyle={{
-                    border: '3px solid #A54B0A'
-                }}
-            />
         </>
     )
 }
